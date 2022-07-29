ab95a839132e759bc672494ba169a308c382fa09
EquiliTeam <office@e-quili.com>
2022-07-29 06:52:03 +0200

    changing text

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">Monsters Rolodex 1</h1>
       <SearchBox
         className="monsters-search-box"
         onChangeHandler={onSearchChange}
